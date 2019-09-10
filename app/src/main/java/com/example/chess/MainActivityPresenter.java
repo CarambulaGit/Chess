632d3d154b19ad07965900a05feb798e1afa506a
Carambula <antibog6@gmail.com>
2019-09-10 16:20:29 +0300

    delete useless fields

--- app/src/main/java/com/example/chess/MainActivityPresenter.java
+++ app/src/main/java/com/example/chess/MainActivityPresenter.java
@@ -49,85 +49,21 @@ public class MainActivityPresenter {
                 count++;
             }
         }
-        cell[1].id = R.id.a1;
         cell[1].figure = Figure.CASTLE;
-        cell[2].id = R.id.b1;
         cell[2].figure = Figure.KNIGHT;
-        cell[3].id = R.id.c1;
         cell[3].figure = Figure.BISHOP;
-        cell[4].id = R.id.d1;
         cell[4].figure = Figure.QUEEN;
-        cell[5].id = R.id.e1;
         cell[5].figure = Figure.KING;
-        cell[6].id = R.id.f1;
         cell[6].figure = Figure.BISHOP;
-        cell[7].id = R.id.g1;
         cell[7].figure = Figure.KNIGHT;
-        cell[8].id = R.id.h1;
         cell[8].figure = Figure.CASTLE;
-        cell[9].id = R.id.a2;
-        cell[10].id = R.id.b2;
-        cell[11].id = R.id.c2;
-        cell[12].id = R.id.d2;
-        cell[13].id = R.id.e2;
-        cell[14].id = R.id.f2;
-        cell[15].id = R.id.g2;
-        cell[16].id = R.id.h2;
-        cell[17].id = R.id.a3;
-        cell[18].id = R.id.b3;
-        cell[19].id = R.id.c3;
-        cell[20].id = R.id.d3;
-        cell[21].id = R.id.e3;
-        cell[22].id = R.id.f3;
-        cell[23].id = R.id.g3;
-        cell[24].id = R.id.h3;
-        cell[25].id = R.id.a4;
-        cell[26].id = R.id.b4;
-        cell[27].id = R.id.c4;
-        cell[28].id = R.id.d4;
-        cell[29].id = R.id.e4;
-        cell[30].id = R.id.f4;
-        cell[31].id = R.id.g4;
-        cell[32].id = R.id.h4;
-        cell[33].id = R.id.a5;
-        cell[34].id = R.id.b5;
-        cell[35].id = R.id.c5;
-        cell[36].id = R.id.d5;
-        cell[37].id = R.id.e5;
-        cell[38].id = R.id.f5;
-        cell[39].id = R.id.g5;
-        cell[40].id = R.id.h5;
-        cell[41].id = R.id.a6;
-        cell[42].id = R.id.b6;
-        cell[43].id = R.id.c6;
-        cell[44].id = R.id.d6;
-        cell[45].id = R.id.e6;
-        cell[46].id = R.id.f6;
-        cell[47].id = R.id.g6;
-        cell[48].id = R.id.h6;
-        cell[49].id = R.id.a7;
-        cell[50].id = R.id.b7;
-        cell[51].id = R.id.c7;
-        cell[52].id = R.id.d7;
-        cell[53].id = R.id.e7;
-        cell[54].id = R.id.f7;
-        cell[55].id = R.id.g7;
-        cell[56].id = R.id.h7;
-        cell[57].id = R.id.a8;
         cell[57].figure = Figure.CASTLE;
-        cell[58].id = R.id.b8;
         cell[58].figure = Figure.KNIGHT;
-        cell[59].id = R.id.c8;
         cell[59].figure = Figure.BISHOP;
-        cell[60].id = R.id.d8;
         cell[60].figure = Figure.QUEEN;
-        cell[61].id = R.id.e8;
         cell[61].figure = Figure.KING;
-        cell[62].id = R.id.f8;
         cell[62].figure = Figure.BISHOP;
-        cell[63].id = R.id.g8;
         cell[63].figure = Figure.KNIGHT;
-        cell[64].id = R.id.h8;
         cell[64].figure = Figure.CASTLE;
     }
 
@@ -192,6 +128,7 @@ public class MainActivityPresenter {
         if (Math.abs(deltaX) <= 1
                 && Math.abs(deltaY) <= 1)
             return cleanWay(cell[indFig].x, cell[indFig].y, deltaX, deltaY, cell[indFig].figType);
+        //todo castling
         return false;
     }
 
@@ -231,7 +168,7 @@ public class MainActivityPresenter {
     public boolean cleanWay(int coordinateX, int coordinateY, int deltaX, int deltaY, FigType figType) {
         int x = coordinateX;
         int y = coordinateY;
-        for (int i = 0; i < Math.abs(deltaX) - 1; i++) {
+        for (int i = 0; i < Math.max(Math.abs(deltaX), Math.abs(deltaY)) - 1; i++) {
             if (deltaX > 0) {
                 x++;
             } else {
